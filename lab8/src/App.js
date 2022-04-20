c1e6e1a0b8eef2a6b8344e12d245a77513a3a533
DAA412 <sasha.dolinskii.02@gmail.com>
2022-04-20 18:21:05 +0300

    lab8 ready

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from "./components/TodoList.jsx";
+import Context from "./context";
+import AddItem from "./components/AddItem.jsx";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [todos, setTodos] = React.useState([]);
 
-export default App;
+    function toggleTodo(id) 
+    {
+        setTodos(todos.map(todo => 
+          {
+            if (todo.id === id) { todo.completed = !todo.completed }
+            return todo;
+        }))
+    }
+
+    function editTodo(id, newTitle)
+    {
+        setTodos(
+            todos.map((todo) => 
+            {
+                if (todo.id === id) todo.title = newTitle;
+                return todo;
+            })
+        );
+    }
+
+    function createTodo(title) 
+    {
+        setTodos(
+            todos.concat([{
+                title,
+                id: Date.now(),
+                completed: false,
+            }, ])
+        );
+    }
+    function removeTodo(id) { setTodos(todos.filter(todo => todo.id !== id)) }
+    return ( 
+    <div class="main">.
+        <Context.Provider value = {{ removeTodo, editTodo }} >
+          <div className = "wrapper" >
+          <h1 class="header">Список тудушек</h1>
+          <AddItem onCreate = { createTodo } />  
+          {
+            todos.length ? < TodoList todos = { todos }
+            onToggle = { toggleTodo } /> : <p class="foot">Пока дел нет:)</p >
+          }
+
+        </div>
+      </Context.Provider>
+    .</div>
+);
+}
+export default App 
